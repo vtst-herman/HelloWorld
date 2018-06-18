8912e869b137496307d7315f1ac92a269531fc18
vtstherman <vtstherman@gmail.com>
2018-06-18 10:10:32 +0200

    bewaar

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Herman!");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
